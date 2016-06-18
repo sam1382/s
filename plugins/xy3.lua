40222b8020ab45e2a0a7b634b474425847b33544
sam1382 <samiiiiiiiiiiiiiiiiiiiiiiii24@gmail.com>
2016-06-17 22:57:24 -0700

    Update xy3.lua

--- plugins/xy3.lua
+++ plugins/xy3.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches)
-local bot_id = 217170905
+local bot_id = 231333287
 local fbot1 = 199063048
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
